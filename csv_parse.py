07e92cda64eea971c84fc71aee0e9f0acd384648
fractal2k <37149174+fractal2k@users.noreply.github.com>
2018-10-09 09:36:04 +0530

    Added functionality
    Added function to convert only the required part of the PDF to CSV

--- csv_parse.py
+++ csv_parse.py
@@ -1,12 +1,14 @@
-import csv, os, sys, constants
+import csv, os, sys
+import constants as c
 
-# will need to plugin the coordinates
-# tl =  39.6, 156.2
-# br = 936.0, 441.4
-os.system("java -jar tabula.jar -p all -o {0}.csv {0}.pdf".format(sys.argv[1]))
+def convert2csv(filename):
+    os.system(
+        f'java -jar tabula-1.0.2-jar-with-dependencies.jar -p all -a {c.Y1},{c.X1},{c.Y2},{c.X2} -o {filename}.csv {filename}.pdf')
 
+    
 # placeholder string for each student, append this to the csv in the end after all operations
 result_current_student = ""
+convert2csv(sys.argv[1])
 
 with open(sys.argv[1]) as file:
     reader = list(csv.reader(file))  # list where one list member comprises of one row of the csv
@@ -14,6 +16,3 @@ with open(sys.argv[1]) as file:
     for row in range(constants.INITIAL_ROW, len(reader), constants.ROWS_PER_PAGE):
         for  student_row in range(0, constants.ROWS_PER_PAGE, constants.LENGTH_EACH_STUDENT):
             # OPERATIONS TO DO HERE
-
-
-

67e48abfce6aaf17f6aaab0849ef8ddb7086768a
fractal2k <37149174+fractal2k@users.noreply.github.com>
2018-10-09 09:29:25 +0530

    Added coordinates of the selection

--- constants.py
+++ constants.py
@@ -1,3 +1,7 @@
 INITIAL_ROW = 0  # initial row per page
 ROWS_PER_PAGE = 0
-LENGTH_EACH_STUDENT = 0  # number of rows comprising the marks details of each student
+LENGTH_EACH_STUDENT = 0 # number of rows comprising the marks details of each student
+Y1 = 156.2  # y coordinate of the top left of the required table
+X1 = 39.6  # x coordinate of the top left of the required table
+Y2 = 441.4  # y coordinate of the bottom right of the required table
+X2 = 936.0  # x coordinate of the bottom right of the required table

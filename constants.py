002d2af57ff3dac4c8fb785c86e95deced300301
Amit Panthi <apapanthi@gmail.com>
2018-10-13 18:20:21 +0530

    Added first subject marks, fixed loop, few tweaks

--- constants.py
+++ constants.py
@@ -1,6 +1,6 @@
 INITIAL_ROW = 0  # initial row per page
-ROWS_PER_PAGE = 0
-LENGTH_EACH_STUDENT = 0 # number of rows comprising the marks details of each student
+ROWS_PER_PAGE = 24
+LENGTH_EACH_STUDENT = 4  # number of rows comprising the marks details of each student
 Y1 = 156.2  # y coordinate of the top left of the required table
 X1 = 39.6  # x coordinate of the top left of the required table
 Y2 = 441.4  # y coordinate of the bottom right of the required table
--- csv_parse.py
+++ csv_parse.py
@@ -6,47 +6,52 @@ import re
 
 
 def convert2csv(filename):
-    os.system(
-        f'java -jar tabula-1.0.2-jar-with-dependencies.jar -p all -a {constants.Y1},{constants.X1},{constants.Y2},{constants.X2} -o {filename}.csv {filename}.pdf')
+    os.system(f'java -jar tabula-1.0.2-jar-with-dependencies.jar -p all -a {constants.Y1},{constants.X1},{constants.Y2},{constants.X2} -o {filename}.csv {filename}.pdf')
 
 
-# will need to plugin the coordinates
-# tl =  39.6, 156.2
-# br = 936.0, 441.4
+convert2csv(sys.argv[1])
 
 # placeholder string for each student, append this to the csv in the end after all operations
 result_current_student = ""
 
-convert2csv(sys.argv[1])
-
-with open(sys.argv[1]) as file:
-    reader = list(csv.reader(file))  # list where one list member comprises of one row of the csv
-    # will need to think of a way to get number of subjects and subject names
-
-    for line in reader:
-        line = " ".join(line)
-
-    for row in range(constants.INITIAL_ROW, len(reader), constants.ROWS_PER_PAGE):
-        for student_row in range(0, constants.ROWS_PER_PAGE, constants.LENGTH_EACH_STUDENT):
-            name_current_student = reader[row + student_row][0].split()
-            result_current_student += name_current_student[0] + ", " + " ".join(name_current_student)[len(name_current_student[0]):] + ", "
-
 
 def get_marks(row1, row2, row3, row4):
-    marks1 = []
-    marks2 = []
-    marks3 = []
-    marks4 = []
-    marks5 = []
-    marks6 = []
-
+    row1_tokens = row_search(row1)
+    row2_tokens = row_search(row2)
+    row3_tokens = row_search(row3)
+    row4_tokens = row_search(row4)
     # first subject
     index = 0
-    row1_tokens = re.findall("[\.\d\w\(\)\-\+]+", row1)
     for token in row1_tokens:
         match = re.search("\d+", token)
         if match:
             index = row1_tokens.index(token)
             break
 
+    marks1 = row1_tokens[index: index + 5] + row2_tokens[1:4]
+    # row 2 tokens are wrong for now, will printout wrong stuff if name leaks
+    return marks1
 
+
+def row_search(r):
+    tokens = re.findall("[.\d\w()\-+]+", r)
+    return tokens
+
+
+with open(sys.argv[1]) as file:
+    csv_r = list(csv.reader(file))  # list where one list member comprises of one row of the csv
+    # will need to think of a way to get number of subjects and subject names
+    reader = []
+
+    for line in csv_r:
+        current_line = " ".join(line)
+        reader.append(current_line)
+
+    for row in range(constants.INITIAL_ROW, len(reader), constants.ROWS_PER_PAGE):
+        for student_row in range(0, constants.ROWS_PER_PAGE, constants.LENGTH_EACH_STUDENT):
+            name_current_student = reader[row + student_row][0].split()
+            result_current_student += name_current_student[0] + ", " + " ".join(name_current_student)[len(name_current_student[0]):] + ", "
+            sub1_marks = get_marks(reader[student_row + 0], reader[student_row + 1])
+
+            result_current_student = str(sub1_marks)
+            print(result_current_student)
